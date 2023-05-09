6cb7c1a4b19bc844308adc9c32fce0b84d013207
Olena <pampam32121@gmail.com>
2023-05-09 23:56:23 +0300

    downloaded basic project

---
+++ index.js
@@ -0,0 +1,14 @@
+const setupTextarea = document.getElementById('setup-textarea') 
+const setupInputContainer = document.getElementById('setup-input-container')
+const movieBossText = document.getElementById('movie-boss-text')
+
+const apiKey = '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'
+const url = 'https://api.openai.com/v1/completions'
+
+document.getElementById("send-btn").addEventListener("click", () => {
+  if (setupTextarea.value) {
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+  }
+})
+
